b99ebd211adbfbded7d96f90d2bae56cc2193bff
shenaliefdo <shenalie.fdo@gmail.com>
2018-09-23 18:20:29 -0400

    added controllers

--- application/routes/index.js
+++ application/routes/index.js
@@ -1,4 +1,7 @@
 
 const express = require('express');
 const router = express.Router();
+const ProductController = require("../controllers/productsController");
+const OrderController = require("../controllers/ordersController");
+const ShopController = require("../controllers/shopsController");
 module.exports = router;
